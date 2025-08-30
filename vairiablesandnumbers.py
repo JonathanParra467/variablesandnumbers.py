3b803736e816358d38113aad4a0df3f93ce427ba
JonathanParra467 <jparra1786@students.mchenry.edu>
2025-08-29 22:22:10 -0500

    Add files via upload

---
+++ vairiablesandnumbers.py
@@ -0,0 +1,16 @@
+pounds_1 = 65
+pounds_2 = 24
+pounds_3 = 12
+pounds_4 = 34
+
+kilograms_1 = 2.20462 
+
+ounces_1 = pounds_1 * kilograms_1
+ounces_2 = pounds_2 * kilograms_1
+ounces_3 = pounds_3 * kilograms_1
+ounces_4 = pounds_4 * kilograms_1
+
+print(f"{ounces_1} pounds is equal to {ounces_1:.2f} ounces.")
+print(f"{ounces_2} pounds is equal to {ounces_2:.2f} ounces.")
+print(f"{ounces_3} pounds is equal to {ounces_3:.2f} ounces.")
+print(f"{ounces_4} pounds is equal to {ounces_4:.2f} ounces.")
